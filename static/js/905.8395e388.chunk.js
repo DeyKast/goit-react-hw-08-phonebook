f70778a9b8f415d71a2b9a4f01f897e9faf4df12
DeyKast <109087019+DeyKast@users.noreply.github.com>
2023-07-03 10:05:15 +0000

    Deploying to gh-pages from @ DeyKast/goit-react-hw-08-phonebook@c20b78a19ab8d91b09ea5a56f2aa85411ad7988f 🚀

---
+++ static/js/905.8395e388.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[905],{5905:function(e,t,a){a.r(t),a.d(t,{default:function(){return c}});var n="HomePage_homeText__OXu7V",r=a(184),o=["Welcome!","Have a nice day!","Hiya!","Hey, what's up?","It's great to see you!","Long-time, no see."],u=function(){var e=Math.floor(Math.random()*o.length);return o[e]};function c(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{className:n,children:u()})})}}}]);
+//# sourceMappingURL=905.8395e388.chunk.js.map
